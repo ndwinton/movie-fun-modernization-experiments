b4d98ecb0b9ed22a3ca5fdeb329f76eadc9f7a79
Neil Winton <nwinton@pivotal.io>
2019-11-25 16:02:24 +0530

    Update SmokeTest to start server automatically

--- src/test/java/org/superbiz/moviefun/SmokeTest.java
+++ src/test/java/org/superbiz/moviefun/SmokeTest.java
@@ -1,22 +1,31 @@
 package org.superbiz.moviefun;
 
 import org.junit.Test;
-import org.springframework.web.client.RestTemplate;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
 public class SmokeTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
 
     @Test
     public void smokeTest() {
-        RestTemplate restTemplate = new RestTemplate();
 
-        String homePage = restTemplate.getForObject(url("/"), String.class);
+        String homePage = restTemplate.getForObject("/", String.class);
 
         assertThat(homePage, containsString("Please select one of the following links:"));
 
-        String setupPage = restTemplate.getForObject(url("/setup"), String.class);
+        String setupPage = restTemplate.getForObject("/setup", String.class);
 
         assertThat(setupPage, containsString("Wedding Crashers"));
         assertThat(setupPage, containsString("Starsky & Hutch"));
@@ -24,20 +33,9 @@ public class SmokeTest {
         assertThat(setupPage, containsString("I-Spy"));
         assertThat(setupPage, containsString("The Royal Tenenbaums"));
 
-        String movieFunPage = restTemplate.getForObject(url("/moviefun"), String.class);
+        String movieFunPage = restTemplate.getForObject("/moviefun", String.class);
 
         assertThat(movieFunPage, containsString("Wedding Crashers"));
         assertThat(movieFunPage, containsString("David Dobkin"));
-    }
-
-    private String url(String path) {
-        String baseUrl = "http://localhost:8080/";
-        String envUrl = System.getenv("MOVIE_FUN_URL");
-
-        if (envUrl != null && !envUrl.isEmpty()) {
-            baseUrl = envUrl;
-        }
-
-        return baseUrl + path;
     }
 }
